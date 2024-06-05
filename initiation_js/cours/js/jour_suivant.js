7f0e28b494b017656c43c85ae27778c51885cd6a
Greg <gregorylyvonet@lepoles.org>
2024-06-05 16:53:49 +0200

    exercices

---
+++ initiation_js/cours/js/jour_suivant.js
@@ -0,0 +1,36 @@
+var jour = window.prompt("Ecrivez le jour");
+
+var jour1 = "lundi"
+var jour2 = "mardi";
+var jour3 = "mercredi";
+var jour4 = "jeudi";
+var jour5 = "vendredi";
+var jour6 = "samedi";
+var jour7 = "dimanche";
+
+
+if (jour == jour1) {
+    document.write(jour2);
+} else if (jour == jour2){
+  document.write(jour3);
+}
+
+else if (jour == jour2) {
+  document.write(jour3);
+} else if (jour == jour3){
+  document.write(jour4);
+}
+
+else if (jour == jour3) {
+  document.write(jour4);
+} else if (jour == jour4){
+  document.write(jour5);
+}
+
+else if (jour == jour5) {
+    document.write(jour6)
+} else if (jour == jour6) {
+  document.write(jour7);
+}
+
+
